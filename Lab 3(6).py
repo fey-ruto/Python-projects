521ba1c1ff89511480837e6ddc390cc6635d0237
Faith Ruto <faith.ruto@ashesi.edu.gh>
2025-09-16 18:33:29 +0000

    Delete Lab 3(6).py

--- Lab 3(6).py
+++
@@ -1,6 +0,0 @@
-year_name = int(input("Please enter a year: "))
-
-if (year_name % 400 == 0) or (year_name % 4 == 0 and year_name % 100 != 0):
-    print("This is a leap year.")
-else:
-    print("This is not a leap year.")
