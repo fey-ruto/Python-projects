74b1c880864d3ed9ac062ce3226eb5fef8113d90
Faith Ruto <faith.ruto@ashesi.edu.gh>
2025-09-16 18:28:20 +0000

    Delete Lab1(Problem 1)2.py

--- Lab1(Problem 1)2.py
+++
@@ -1,6 +0,0 @@
-Ta = int(input("Kindly enter the air temprature"))
-V = int(input("Kindly enter the wind speed"))
-
-WCI = 13.12 + 0.6215Ta  - 11.37V ** 0.16 + 0.3965TaV ** 0.16
-
-Print("The wind chill index is:" + str (WCI))
